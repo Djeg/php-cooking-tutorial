03587853d603393f59041902cf6d82a41c0e2b15
David Jegat <david.jegat@gmail.com>
2022-10-18 10:02:14 +0200

    Correction exerice 3

--- exercices/exercice3.php
+++ exercices/exercice3.php
@@ -23,3 +23,59 @@ son numéro (1 pour la note 1, 2 pour la note 2 etc ...)
 Toujours en utilisant une balise ul et li, afficher uniquement
 les notes au dessus ou égale à la moyenne !
 -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 3</title>
+</head>
+
+<body>
+    <h1>Notes de l'éléve</h1>
+
+    <ul>
+        <!--
+        En php il éxiste des "short open tag", des balises
+        ouvrante et fermante plus simple à écrire :
+
+        Par éxemple :
+        <?php $test = 3; ?>
+
+        C'est la même chose que :
+        <? $test = 3; ?>
+        -->
+
+        <!--
+        Lorsque l'on fais du php à l'intérieur d'un fichier
+        html, on s'apercois que notre code PHP n'est pas facile à lire :
+
+        <? foreach ($notes as $index => $note) { ?>
+            <li>Note n°<?= $index ?> : <?= $note ?></li>
+        <? } ?>
+
+        Il éxiste une deuxième technique pour faire des conditions (if, else ...),
+        des boucles (while, for, foreach ...).
+
+        On nomme cette technique "template syntax", cela permet de rendre
+        notre code beaucoup plus lisible lorsque l'on mélange PHP à du
+        HTML
+        -->
+
+        <? foreach ($notes as $index => $note) : ?>
+            <li>Note n°<?= $index + 1 ?> : <?= $note ?></li>
+        <? endforeach ?>
+    </ul>
+
+    <ul>
+        <? foreach ($notes as $index => $note) : ?>
+            <? if ($note >= 10) : ?>
+                <li>Note n°<?= $index + 1 ?> : <?= $note ?></li>
+            <? endif ?>
+        <? endforeach ?>
+    </ul>
+</body>
+
+</html>
