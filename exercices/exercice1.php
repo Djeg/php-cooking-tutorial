c976fd5b1c4fcb99d784d6850ff95e8cd9cb2788
David Jegat <david.jegat@gmail.com>
2022-10-18 09:23:47 +0200

    Correction de l'exercice 1

--- exercices/exercice1.php
+++ exercices/exercice1.php
@@ -4,5 +4,24 @@ Créer un fichier php nommé "exercice1.php".
 Dans ce fichier créer une variable "nom" avec la valeur
 de votre choix.
 
-Afficher la nom dans une balise h1.
+Afficher le nom dans une balise h1.
 -->
+<?php
+$nom = 'John';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 1</title>
+</head>
+
+<body>
+    <h1><?php echo $nom; ?></h1>
+</body>
+
+</html>
