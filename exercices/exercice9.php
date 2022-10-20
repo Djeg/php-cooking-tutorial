6f28d45c28e8adb29e7fd89878560ec6525e5d44
David Jegat <david.jegat@gmail.com>
2022-10-20 14:29:02 +0200

    Correction

--- exercices/exercice9.php
+++ exercices/exercice9.php
@@ -5,7 +5,8 @@ Remplacer l'utilisation de la session par des cookie !!!
 Donc :
 - Plus besoin de session_start
 - Plus besoin de session destroy
-- utiliser setcookie pour enregistrer l'utilisateur dans les cookie
+- utiliser setcookie pour enregistrer
+  l'utilisateur dans les cookie (6 mois d'expiration)
 - utiliser setcookie pour le supprimer (le déconnécté)
 - Remplacer la superglobal $_SESSION par $_COOKIE
 */
